daeb1f723fabcbd0a7e7061afc6bec7dc15e38ae
Lucas Drapier <lucas.drapier@gmail.com>
2020-05-19 02:26:58 +0200

    fix header responsive

--- src/Components/SimpleComponents/Header/Header.js
+++ src/Components/SimpleComponents/Header/Header.js
@@ -22,14 +22,24 @@ const Header = props => {
     return (
         <Fragment>
             {desktop &&
-            <div className='header desktop'>
-                <div className={`rankingButton ${openSvg == true && 'openSvg'}`} onClick={ () => openRanking()}>
-                    <p>Voir le classement</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" fill="white"/></svg>
+            <Fragment>
+                <div className='header desktop'>
+                    <div className={`rankingButton ${openSvg == true && 'openSvg'}`} onClick={ () => openRanking()}>
+                        <p>Voir le classement</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" fill="white"/></svg>
+                    </div>
+                    <p>{props.nmbVote} votes</p>
+                    <h1>CATMASH</h1>
                 </div>
-                <p>{props.nmbVote} votes</p>
-                <h1>CATMASH</h1>
-            </div>
+                <div className='header responsive'> 
+                    <div className={`rankingButton ${openSvg == true && 'openSvg'}`} onClick={ () => openRanking()}>
+                        <p>Voir le classement</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" fill="white"/></svg>
+                    </div>
+                    <h1>CATMASH</h1>
+                    <p>{props.nmbVote} votes</p>
+                </div>
+            </Fragment>
             }
             { responsive &&
                 <div className='header responsive'> 
